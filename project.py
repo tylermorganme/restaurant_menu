f31856b1b168a7d447e64f3a4533bdffd90a5be9
Tyler Morgan <tyler@tylermorgan.me>
2015-07-28 21:13:05 -0700

    initial commit. Functional menu app. EO Lessons

---
+++ project.py
@@ -0,0 +1,92 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+app = Flask(__name__)
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from database_setup import Restaurant, Base, MenuItem
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+# Bind the engine to the metadata of the Base class so that the
+# declaratives can be accessed through a DBSession instance
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+# A DBSession() instance establishes all conversations with the database
+# and represents a "staging zone" for all the objects loaded into the
+# database session object. Any change made against the objects in the
+# session won't be persisted into the database until you call
+# session.commit(). If you're not happy about the changes, you can
+# revert all of them back to the last commit by calling
+# session.rollback()
+session = DBSession()
+
+@app.route('/')
+def defaultRestaurantMenu():
+	restaurant = session.query(Restaurant).first()
+	items = session.query(MenuItem).filter_by(
+		restaurant_id=restaurant.id)
+	return render_template('menu.html', restaurant=restaurant, items=items)
+
+@app.route('/restaurants/<int:restaurant_id>/')
+def restaurantMenu(restaurant_id):
+	restaurant = session.query(Restaurant).filter_by(
+		id=restaurant_id).one()
+	items = session.query(MenuItem).filter_by(
+		restaurant_id=restaurant.id)
+	return render_template('menu.html', restaurant=restaurant, items=items)
+
+@app.route('/restaurants/<int:restaurant_id>/new/', methods=['GET', 'POST'])
+def newMenuItem(restaurant_id):
+	if request.method == 'POST':
+		newItem = MenuItem(name = request.form['name'], restaurant_id = restaurant_id)
+		session.add(newItem)
+		session.commit()
+		flash("new menu item create!")
+		return redirect(url_for('restaurantMenu', restaurant_id = restaurant_id))
+	else:
+		return redirect(url_for('newmenuitem.html', restaurant_id = restaurant_id))
+
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit/', methods = ['GET', 'POST'])
+def editMenuItem(restaurant_id, menu_id):
+	editedItem = session.query(MenuItem).filter_by(id = menu_id).one()
+	previousName = editedItem.name
+	if request.method == 'POST':
+		editedItem.name = request.form['name']
+		session.add(editedItem)
+		session.commit()
+		flash("Changed name form " + previousName + " to " + editedItem.name)
+		return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+	else:
+		return render_template('editmenuitem.html', restaurant_id = restaurant_id, menu_id = menu_id, item = editedItem)
+
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete/', methods = ['GET', 'POST'])
+def deleteMenuItem(restaurant_id, menu_id):
+	deleteItem = session.query(MenuItem).filter_by(id = menu_id).one()
+	deletedItemName = deleteItem.name
+	if request.method =='POST':
+		session.delete(deleteItem)
+		session.commit()
+		flash("Deleted " + deletedItemName)
+		return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+	else:
+		return render_template('deletemenuitem.html', restaurant_id = restaurant_id, menu_id = menu_id, item = deleteItem)
+
+#Making an API Endpoint (GET Request)
+@app.route('/restaurants/<int:restaurant_id>/menu/JSON')
+def restaurantMenuJSON(restaurant_id):
+	restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+	items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
+	return jsonify(MenuItems=[i.serialize for i in items])
+
+@app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/JSON')
+def menuItemJSON(restaurant_id, menu_id):
+	restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+	item = session.query(MenuItem).filter_by(id=menu_id).one()
+	return jsonify(MenuItems=item.serialize)
+
+
+if __name__ == '__main__':
+	app.secret_key = 'super_secret_key'
+	app.debug = True
+	app.run(host='0.0.0.0', port=5000)
---
+++ test.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+myFirstRestaurant = Restaurant(name = "Pizza Palace")
+session.add(myFirstRestaurant)
+session.commit()
+session.query(Restaurant).all()
+cheesepizza = MenuItem(name="Cheese Pizza", description="Made with all natural ingredients and fresh mozzarella", course="Entree", price="$8.99", restaurant=myFirstRestaurant)
+session.add(cheesepizza)
+session.commit()
+session.query(MenuItem).all()
